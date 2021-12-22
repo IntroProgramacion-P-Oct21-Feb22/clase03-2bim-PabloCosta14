c74498477f6d344eb25e8964668f632dc2296c29
Pablo Costa <pacosta2@utpl.edu.ec>
2021-12-22 10:33:04 -0500

    commit

--- Ejemplosc3/src/paquete01/Ejemplo04.java
+++ Ejemplosc3/src/paquete01/Ejemplo04.java
@@ -29,10 +29,13 @@ public class Ejemplo04 {
         
         int suma;
         
-        for (int fila = 0; fila < arreglo1.length; fila++) {
+        for (int fila = 0; fila < arreglo1.length; fila++) { // 0<2
             suma = 0;
-            for (int col = 0; col < arreglo1[fila].length; col++) {
-                suma = suma + arreglo1[fila][col];
+            for (int col = 0; col < arreglo1[fila].length; col++) { // arreglo[0].length:3
+                suma = suma + arreglo1[fila][col];                 // 0<3 // 1<3
+                // 0 + arreglo[0][0] //0+10//10
+                //10 + arreglo1[0][01] // 10+20//30
+                //30 + arreglo[0][2] // 30+50 // 70
             }
             System.out.printf("Suma de valores del arreglo: %d\n", suma);
         }
@@ -41,3 +44,10 @@ public class Ejemplo04 {
     }
     
 }
+/*
+fila:0
+col:0, 1, 2, 3
+Suma:0, 30, 70
+pantalla:0
+suma de valores del arreglo : 70
+*/
